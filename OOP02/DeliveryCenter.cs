1469de28789c919bcbfa504878e9f2d0d7e9c8d6
GannaAttia <gannaattia305@gmail.com>
2026-08-09 16:59:11 +0300

    Methods : PrintAllShipments

--- OOP02/DeliveryCenter.cs
+++ OOP02/DeliveryCenter.cs
@@ -103,6 +103,27 @@ namespace OOP02
             }
             return false;
         }
+
+
+        public void PrintAllShipments()
+        {
+            Console.WriteLine($"Shipments in {CenterName}:");
+            for (int i = 0; i < shipments.Length; i++)
+            {
+                Shipment shipment = shipments[i];
+                if (shipment != null)
+                {
+                    Console.WriteLine($"Shipment {i + 1}:");
+                    Console.WriteLine($"Tracking Code: {shipment.TrackingCode}");
+                    Console.WriteLine($"Description: {shipment.Description}");
+                    Console.WriteLine($"Weight: {shipment.Weight} kg");
+                    Console.WriteLine($"Delivery Fee: ${shipment.DeliveryFee}");
+                    Console.WriteLine($"Destination: {shipment.Destination}");
+                    Console.WriteLine($"Estimated Cost: ${shipment.EstimatedCost}");
+                    Console.WriteLine();
+                }
+            }
+        }
         #endregion
     }
 }
